96da4b1b22b25aa3a0cca6f4d88ec22161d7d608
mouthzipper <ramos.jeromeh@gmail.com>
2020-04-10 16:33:19 +0800

    chore: simple content

---
+++ pages/_app.tsx
@@ -0,0 +1,42 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { Flex, Box} from 'rebass';
+import Footer from '../components/Footer';
+
+
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+        <Head>
+          <title>Jerome Ramos</title>
+          <link rel="icon" href="/favicon.png" />
+          <link
+            href="https://fonts.googleapis.com/css?family=Poppins:300&display=swap"
+            rel="stylesheet"
+          />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </Head>
+        <Flex flexDirection="column">
+          <Box>
+            <Component {...pageProps} />
+          </Box>
+          <Footer />
+        </Flex>
+    <style jsx global>{`
+      html,
+      body {
+        padding: 0;
+        margin: 0;
+        font-family: 'Poppins', -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+      }
+      * {
+        box-sizing: border-box;
+      }
+    `}</style>
+  </>
+  );
+}
+
+export default App;
