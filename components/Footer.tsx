764c22afd8fe42c90bde529c6cea5f151aebdff6
Jerome Ramos <ramos.jeromeh@gmail.com>
2021-01-16 07:49:48 +0800

    Bump

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,8 @@
-import { Box, Text } from 'rebass'
+import { Box, Text } from "rebass";
 export default function () {
-  return <Box p={[4, 5, 6]} color="#4B4B4B">
-    <Text fontSize={18}>© 2020</Text>
+  return (
+    <Box p={[4, 5, 6]} color="#4B4B4B">
+      <Text fontSize={18}>© 2021</Text>
     </Box>
+  );
 }
